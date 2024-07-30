9540f79d10cfb903a39487ddbc028c81a1b3eed5
jayantverma11 <148342663+jayantverma11@users.noreply.github.com>
2024-07-30 15:34:56 +0530

    Update inputs.tf

--- templates/eks/inputs.tf
+++ templates/eks/inputs.tf
@@ -3,3 +3,8 @@ module "eks" {
   version = "19.15.4"
   # other configuration
 }
+
+module "cluster_autoscaler_irsa_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version   = "5.28.0"
+}
